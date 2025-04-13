8cdee11475c6f3e29cd0aad4f625ebaaa13abf54
David <davidbuckleyweb@gmail.com>
2025-04-13 18:12:39 +0100

    Initial Commit

---
+++ Cella/CellaApp.swift
@@ -0,0 +1,32 @@
+//
+//  CellaApp.swift
+//  Cella
+//
+//  Created by david on 13/04/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CellaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CellaTests/CellaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CellaTests.swift
+//  CellaTests
+//
+//  Created by david on 13/04/2025.
+//
+
+import Testing
+@testable import Cella
+
+struct CellaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
